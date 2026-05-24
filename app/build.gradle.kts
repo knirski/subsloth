855250560d4f01ae7a5ecc7ae79db5edbf11773c
knirski <krzysztof.nirski+github@gmail.com>
2026-05-24 21:28:01 +0200

    chore: name APK subsloth-{version}-{buildType}.apk instead of app-{buildType}.apk (#7)
    * chore: name APK subsloth-{version}-{buildType}.apk instead of app-{buildType}.apk
    
    * chore: use appVersionName instead of output.versionName.get() in APK naming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,16 @@ android {
             )
         }
     }
+
+    androidComponents {
+        onVariants { variant ->
+            variant.outputs.forEach { output ->
+                output.outputFileName.set(
+                    "subsloth-$appVersionName-${variant.buildType}.apk",
+                )
+            }
+        }
+    }
 }
 
 dependencies {
