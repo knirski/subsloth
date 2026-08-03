016297afe1b6617fadb1366e4e4d8f00b94af356
Krzysztof Nirski <krzysztof.nirski+github@gmail.com>
2026-08-03 12:37:02 +0200

    docs: identify nixpkgs Chromium source

--- flake.nix
+++ flake.nix
@@ -472,7 +472,7 @@
         KOTLIN_YARN_HOME = "${pkgs.yarn}";
         KOTLIN_BINARYEN_HOME = "${pkgs.binaryen}";
         # Kotlin/Wasm's Karma runner uses ChromeHeadless. Point it at the
-        # Nix-provided browser so local tests do not depend on a host install.
+        # nixpkgs chromium package so tests do not depend on a host install.
         CHROME_BIN = "${pkgs.chromium}/bin/chromium";
 
         shellHook = ''
